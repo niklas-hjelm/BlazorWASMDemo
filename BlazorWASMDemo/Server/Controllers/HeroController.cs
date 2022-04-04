e9dc443e4c4a97b184300ee757befef5e9b5baf1
niklas-hjelm <niklas.hjelm@iths.se>
2022-04-04 11:06:26 +0200

    Add project files.

---
+++ BlazorWASMDemo/Server/Controllers/HeroController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWASMDemo.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
+    {
+        private readonly HeroRepository _heroRepository;
+
+        public HeroController(HeroRepository heroRepository)
+        {
+            _heroRepository = heroRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<Hero>>> GetHeroes()
+        {
+            var heroes = await _heroRepository.GetAllHeroesAsync();
+            return Ok(heroes);
+        }
+    }
+}
---
+++ BlazorWASMDemo/Server/Program.cs
@@ -0,0 +1,46 @@
+global using BlazorWASMDemo.Shared;
+global using BlazorWASMDemo.Server.DAL;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<HeroContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HeroConnectionString"));
+});
+
+builder.Services.AddScoped<HeroRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
